190beceea0118a1dccec76496cf598611d309ede
BURAK EMRE İLERİ <21120205050@ismu.edu.tr>
2024-12-21 20:26:45 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'login_screen.dart';
 import 'firebase_options.dart';
 
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
